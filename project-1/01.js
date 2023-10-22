e28963f0f01f420c9eee64ab43967e0fcd7343d4
민권 <chlalsrnjs12@gmail.com>
2023-10-22 22:31:00 +0900

    완성본#1

--- project-1/01.js
+++
@@ -1,9 +0,0 @@
-// tmdb API
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzcxMGNlNDc1ZjNlMDIwMzkwZjBiYzc0MDYwNzA3ZSIsInN1YiI6IjY1MmZhZmQxZWE4NGM3MDBjYTEyZjFmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dfrsiunDYQZ9rdePZjzs14Z-zSc_1WRMPnO2BAIZ-Tc'
-    }
-  };
-  
